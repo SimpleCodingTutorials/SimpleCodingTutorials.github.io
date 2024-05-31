46d04760eda6ee45d771202d0535af2de85261c9
SimpleCodingTutorials <128899490+SimpleCodingTutorials@users.noreply.github.com>
2024-05-31 23:28:09 +0330

    1

--- main.js
+++ main.js
@@ -165,7 +165,7 @@ window.onloadTurnstileCallback = function () {
       callback: function(token) {
           console.log(`Challenge Success ${token}`);
           document.querySelector(".container").style.display = "flex";
-          document.getElementById("mywidget").style.display = "none";
+          document.getElementById("myWidget").style.display = "none";
       },
   });
 };
